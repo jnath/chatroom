07913e92aa153f2b4fce3e31a7a58eaeea54e71d
Jonathan Faivre <jonathan.faivre@radiofrance.com>
2022-05-12 23:27:24 +0200

    add storybook with vite

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+const { mergeConfig } = require('vite');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf"
+  ],
+  "framework": "@storybook/svelte",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "svelteOptions": {
+    "preprocess": import("../svelte.config.js").preprocess
+  },
+  viteFinal: async (config, { configType }) => {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: {
+          $components: '/src/components',
+          $directives: '/src/directives',
+          $models: '/src/models',
+          $stores: '/src/stores',
+          $helpers: '/src/helpers',
+        },
+      },
+    });
+  },
+}
